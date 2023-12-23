700c66f03be19ef28c7a860e1382ce842e8d2d2c
Sergij Dekht <mistersergey055@gmail.com>
2023-12-23 19:17:26 +0100

    Add functions to search and for IO handling

--- src/list.h
+++ src/list.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <errno.h>
+#include <string.h>
+
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
@@ -21,9 +24,13 @@ typedef enum _deallocation_mode {
     STRONG
 } DeallocationMode;
 
-#define LIST_MALLOC_ITEM(type) ((type *)malloc(sizeof(type)))
+#define LIST_MALLOC_ITEMS(type, count) ((type *)malloc(sizeof(type) * count))
+#define LIST_MALLOC_ITEM(type) LIST_MALLOC_ITEMS(type, 1)
 
 #define LIST_SET_ERRNO_END_RETURN(errno_value, return_value) ({ errno = errno_value; return return_value; })
+
+#define LIST_PRINT_NODE_VALUE_AS_INT(node) (printf("%d\n", *(int *)node->data))
+#define LIST_PRINT_NODE_VALUE_AS_STRING(node) (printf("%s\n", (char *)node->data))
 
 /* -- NODE ALLOCATION FUNCTIONS -- */
 
@@ -92,7 +99,67 @@ bool list_deallocate(List **root, DeallocationMode mode);
 
 /* -- -- */
 
-#ifdef LIST_IMPLEMENTATION
+/* -- SEARCHING FUNCTIONS --*/
+
+/// @brief returns a pointer to a node from the list according to its `index` (starts from `0`).
+/// @param root a pointer to to the beginning of the list.
+/// @param index a target node's index.
+/// @return if the node is found, a pointer to it is returned. If the `root` pointer is `NULL`,
+/// the `errno` value is set to `EINVAL` and `NULL` is returned. If index is greater than the
+/// size of the list, then the `NULL` pointer is also returned. 
+List *list_get_by_index(List **root, size_t index);
+
+/// @brief returns a pointer to a node that stores the same integer as in `value`.
+/// `WARNING: if the list contains not only integers, it may cause a segfault!`
+/// @param root a pointer to the beginning of the list.
+/// @param value an integer value that is going to be looked for in a list.
+/// @return a pointer to a node that in which the `data` field stores a pointer to an integer with a 
+/// specific `value`. If either the `root` is `NULL` or the list is empty or the `value` wasn't found, `NULL` is returned.
+List *list_get_by_integer_value(List **root, int value);
+
+/// @brief returns a pointer to a node that stores the same string as in `value`.
+/// `WARNING: if the list contains not only strings, it may cause a segfault!`
+/// @param root a pointer to the beginning of the list.
+/// @param value a string pointer that is going to be compared using the strcmp function.
+/// @return a pointer to a node that in which the `data` field stores a pointer to a strjng with a 
+/// specific `value`. If either the `root` is `NULL` or the list is empty or the `value` wasn't found, `NULL` is returned.
+List *list_get_by_string_value(List **root, char *string);
+
+/// @brief returns a pointer to the last element of the list.
+/// @param root a pointer to the beginning of the memory.
+/// @return a pointer to the last node. If the `root` pointer is `NULL` or the list is empty, 
+/// `NULL` is returned and the `errno` is set to be `EINVAL`.
+List *list_get_last(List **root);
+
+/* -- -- */
+
+/* -- IO FUNCTIONS -- */
+
+/// @brief reads a line from a user and returns a node with the data as a char pointer.
+/// Note: a string is dynamically allocated so when the node is going to be freed, use the `STRONG` mode.
+/// For more information, check the `_deallocation_mode` struct definition.
+/// @param buffer_size the maximum length for the input string.
+/// @return a node with the input as a string. If any errors happen, 
+/// the `errno` is set to be either `EINVAL` (`buffer_size` is zero) or 
+/// `ENOMEM` (failed to allocate memory for a node or an input string) or
+/// `EIO`(failed to read line) and the `NULL` is returned.
+List *list_read_line_as_string(size_t buffer_size);
+
+/// @brief reads `count` lines with the `buffer_size` length and creates a list of the read input lines.
+/// Note: for every read this functions uses the `list_read_line_as_string` function meaning that the list
+/// will contain dynamically allocated strings in each node. So when the list is no longer needed and the 
+/// memory should be released, the `STRONG` deallocation mode is required. 
+/// Check the `_deallocation_mode` struct definition for more information.
+/// @param buffer_size the size for each string in a node.
+/// @param count the amount of lines to read.
+/// @return If no errors occur, the function returns a list of read lines. Otherwise, the function will
+/// return `NULL` and set the `errno` to be `EIO` or `ENOMEM` or `EINVAL` accordingly to the error. To find
+/// out in which case the `errno` will be changed, check the `list_read_line_as_string` function.
+List *list_read_lines_as_string(size_t buffer_size, size_t count);
+
+/* -- -- */
+
+//#ifdef LIST_IMPLEMENTATION
 
 /* -- NODE ALLOCATION FUNCTIONS -- */
 
@@ -213,4 +280,115 @@ bool list_deallocate(List **root, DeallocationMode mode)
 
 /* -- -- */
 
-#endif // LIST_IMPLEMENTATION
+/* -- SEARCHING FUNCTIONS --*/
+ 
+List *list_get_by_index(List **root, size_t index)
+{
+    if (!root) LIST_SET_ERRNO_END_RETURN(EINVAL, NULL);
+
+    size_t current_index = 0;
+
+    for (List *i = *root; i; i = i->next) {
+        if (index == current_index) {
+            return i;
+        }
+
+        current_index++;
+    }
+
+    return NULL;
+}
+
+List *list_get_by_integer_value(List **root, int value)
+{
+    if (!root) LIST_SET_ERRNO_END_RETURN(EINVAL, NULL);
+
+    for (List *i = *root; i; i = i->next) {
+        if (*(int *)i->data == value) {
+            return i;
+        }
+    }
+
+    return NULL;
+}
+
+List *list_get_by_string_value(List **root, char *string)
+{
+    if (!root) LIST_SET_ERRNO_END_RETURN(EINVAL, NULL);
+
+    for (List *i = *root; i; i = i->next) {
+        if (strcmp((char *)i->data, string) == 0) {
+            return i;
+        }
+    }
+
+    return NULL;
+}
+
+List *list_get_last(List **root)
+{
+    if (!root || !(*root)) LIST_SET_ERRNO_END_RETURN(EINVAL, NULL);
+
+    List *last = *root;
+
+    while (last->next) {
+        last = last->next;
+    }
+
+    return last;
+}
+
+/* -- -- */
+
+/* -- IO FUNCTIONS -- */
+
+List *list_read_line_as_string(size_t buffer_size)
+{
+    if (buffer_size == 0) LIST_SET_ERRNO_END_RETURN(EINVAL, NULL);
+
+    List *result = list_allocate_node(NULL);
+
+    if (!result) LIST_SET_ERRNO_END_RETURN(ENOMEM, NULL);
+
+    char *input_string = LIST_MALLOC_ITEMS(char, buffer_size);
+
+    if (!input_string) LIST_SET_ERRNO_END_RETURN(ENOMEM, NULL);
+
+    if (!fgets(input_string, buffer_size, stdin)) LIST_SET_ERRNO_END_RETURN(EIO, NULL);
+
+    input_string[buffer_size - 1] = '\0';
+
+    result->data = input_string;
+
+    return result;
+}
+
+List *list_read_lines_as_string(size_t buffer_size, size_t count)
+{
+    if (count == 0) LIST_SET_ERRNO_END_RETURN(EINVAL, NULL);
+
+    List *result = list_read_line_as_string(buffer_size);
+
+    if (!result) return NULL;
+
+    List *last = result;
+
+    for (size_t i = 0; i < count - 1; i++) {
+        last->next = list_read_line_as_string(buffer_size);
+
+        if (last->next) {
+            last = last->next;
+            continue;
+        }
+
+        list_deallocate(&result, STRONG);
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    return result;
+}
+
+/* -- -- */
+
+//#endif // LIST_IMPLEMENTATION
