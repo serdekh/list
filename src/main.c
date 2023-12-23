fd73f18012796df8eb66724b4cb72b2e67807126
Sergij Dekht <mistersergey055@gmail.com>
2023-12-23 19:18:06 +0100

    Write an example that finds the biggest integers using IO functions

--- src/main.c
+++ src/main.c
@@ -1,28 +1,39 @@
-#include <stdio.h>
-#include <string.h>
-
 #define LIST_IMPLEMENTATION
 #include "./list.h"
 
+#define N 3
+#define ATOI_FAILED(n, node) (n == 0 && strcmp((const char *)node->data, "0") != 0)
+
 int main()
 {
-    printf("Hello world\n");
+    List *numbers = list_read_lines_as_string(10, N);
+  
+    int max = atoi((const char *)(numbers->data));
 
-    int a = 1;
-    int b = 2;
-    int c = 3;
-
-    List *numbers = NULL;
-
-    list_push_front(&numbers, &a);
-    list_push_front(&numbers, &b);
-    list_push_front(&numbers, &c);
-
-    for (List *i = numbers; i; i = i->next) {
-        printf("%d\n", *(int*)i->data);
+    if (ATOI_FAILED(max, numbers)) {
+        fprintf(stderr, "ERROR: invalid input for the 1th number: `%s`\n", (char *)numbers->data);
+        list_deallocate(&numbers, STRONG);
+        return 1;
     }
+
+    size_t index = 1;
+
+    for (List *i = numbers->next; i; i = i->next) {
+        int temp = atoi((const char *)i->data);
+
+        if (ATOI_FAILED(temp, i)) {
+            fprintf(stderr, "ERROR: invalid input for the %zuth number: `%s`\n", index, (char *)numbers->data);
+            list_deallocate(&numbers, STRONG);
+            return 1;
+        }
+
+        if (temp > max) max = temp;
     
-    list_deallocate(&numbers, WEAK);
+        index++;
+    }
+
+    printf("Max number: %d\n", max);
+    list_deallocate(&numbers, STRONG);
 
     return 0;
 }
